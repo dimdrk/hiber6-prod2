6d1d41eaebdf5378f1ed13148c1e7ce6c0126db5
DimDrk <dimdrk@gmail.com>
2024-09-17 22:36:22 +0300

    sql scripts added

---
+++ src/main/resources/sql/courses.sql
@@ -0,0 +1,15 @@
+USE hiber6prod2db;
+
+INSERT INTO `courses` VALUES 
+	(1,'Java'),
+	(2,'C#'),
+	(3,'Python'),
+	(4,'JavaScript'),
+	(5,'TypeScript'),
+	(6,'Scala'),
+	(7,'Go'),
+	(8,'SQL'),
+	(9,'React'),
+	(10,'Angular');
+    
+    ALTER TABLE courses AUTO_INCREMENT = 11;
---
+++ src/main/resources/sql/teachers.sql
@@ -0,0 +1,10 @@
+USE hiber6prod2db;
+
+INSERT INTO `teachers` VALUES 
+	(1,'Αθανάσιος','Ανδρούτσος',1,NULL),
+	(2,'Μάκης','Καπέτης',2,NULL),
+	(3,'Μάρκος','Καραμπάτσης',5,NULL),
+	(4,'Παναγιώτης','Μόσχος',4,NULL),
+	(5,'Σοφοκλής','Στουραϊτης',8,NULL);
+    
+ALTER TABLE regions AUTO_INCREMENT = 6;
